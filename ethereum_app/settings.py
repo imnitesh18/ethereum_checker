fc71dbe4e23f60a8a679dfecd156551feef031a6
Nitesh Mali <niteshmali@nitesh-mali.local>
2024-01-23 21:44:42 +0530

    settings.py updated

--- ethereum_app/settings.py
+++ ethereum_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0+nsw87__u#9u2)4vo!ybq*_^=0mmll4r2it8_(wzv@&=^^wl0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
